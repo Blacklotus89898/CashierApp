979229d7b09ff18be4f32528d4823f5a99b5a224
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2024-04-24 12:23:59 -0400

    adding spring initialzr for backend

---
+++ cashierApp/src/test/java/alimentation/cashierApp/CashierAppApplicationTests.java
@@ -0,0 +1,13 @@
+package alimentation.cashierApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashierAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
